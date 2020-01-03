d44741c020e3e181555454dcee34e695759d6944
Frank Gerold <FrankETSGerold@gmail.com>
2020-01-03 09:37:18 -0500

    create and destroy, first draft

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -16,4 +16,18 @@ class GamesController < ApplicationController
       render json: GameSerializer.new(games, options)
     end
 
+    def create
+      game = Game.create(score: params[:score], level: 1, player_id: params[player_id])
+      options = {
+        include: [:player]
+      }
+      render json: GameSerializer.new(game, options)
+    end
+
+    def destroy
+      game = Game.find(params[:id])
+      game.destroy
+      render json: GameSerializer.new(game)
+    end
+
 end
--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -16,4 +16,18 @@ class PlayersController < ApplicationController
     render json: PlayerSerializer.new(players, options)
   end
 
+  def create
+    player = Player.find_or_create_by(name: params[:name])
+    options = {
+      include: [:games]
+    }
+    render json: PlayerSerializer.new(player, options)
+  end
+
+  def destroy
+    player = Player.find(params[:id])
+    player.destroy
+    render json: PlayerSerializer.new(player)
+  end
+
 end
